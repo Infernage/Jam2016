9c2a092af659aa4b8548dea0adfd54968d2694f7
Daniel Castro García <dandev237@gmail.com>
2016-07-08 23:07:55 +0200

    Added sketch of EnemyAudition.cs

---
+++ Assets/Scripts/EnemyAudition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAudition : MonoBehaviour
+{
+
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        //If the player is crouched, then nothing happens
+        //If the player is standing, he will be detected
+    }
+}
