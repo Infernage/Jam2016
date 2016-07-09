c4a35b5b4010eabf6014029cb58266acd7bbf6c3
Alberto <albertodelcru@gmail.com>
2016-07-09 13:49:55 +0200

    Sprites, character script

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CharacterScript : MonoBehaviour {
+public class CharacterScript : MonoBehaviour
+{
     public float minObjectsForWin = 5;
     private List<GameObject> objectsCollected = new List<GameObject>();
 
@@ -13,34 +14,40 @@ public class CharacterScript : MonoBehaviour {
         return objectsCollected.Count >= minObjectsForWin;
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float h = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float v = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float h = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //float v = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         //transform.Translate(h, v, 0);
         //transform.Rotate(Vector3.right * Time.deltaTime);
-        if (h > 0)
+        if (Input.GetKey(KeyCode.D))
         {
             transform.eulerAngles = new Vector3(0, 0, 270);
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        else if (h < 0)
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.eulerAngles = new Vector3(0, 0, 90);
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        else if (v > 0)
+        else if (Input.GetKey(KeyCode.W))
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        else if (v < 0)
+        else if (Input.GetKey(KeyCode.S))
         {
             transform.eulerAngles = new Vector3(0, 0, 180);
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        transform.Translate(Vector3.forward);
+
 
     }
 
---
+++ Assets/Scripts/ImportantObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImportantObject : MonoBehaviour {
+
+    public float minimum = 0.5f;
+    public float maximum = 1f;
+    public float duration = 5.0f;
+    private float startTime;
+    private SpriteRenderer sprite;
+    void Start()
+    {
+        sprite = this.GetComponent<SpriteRenderer>();
+        startTime = Time.time;
+    }
+    void Update()
+    {
+        float t = (Time.time - startTime) / duration;
+        sprite.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(minimum, maximum, t));
+    }
+}
