e75228c836a8c69d7a20e9e99721b13fc4b20389
Rubenrj <ruben.rj92@gmail.com>
2016-07-09 19:20:32 +0200

    Uso el método de visión del guardia para que el ladrón pueda coger los objetos siempre que los esté mirando.

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -6,8 +6,9 @@ public class CharacterScript : MonoBehaviour
 {
     public float minObjectsForWin = 5;
     private List<GameObject> objectsCollected = new List<GameObject>();
-
     public float speed = 5;
+    public float visionDistance = 10, visionAngle = 20;
+    private GameObject goWhatISee;
 
     public bool HasMinObjects()
     {
@@ -27,35 +28,80 @@ public class CharacterScript : MonoBehaviour
         //float v = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         //transform.Translate(h, v, 0);
         //transform.Rotate(Vector3.right * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.eulerAngles = new Vector3(0, 0, 270);
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.eulerAngles = new Vector3(0, 0, 90);
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.eulerAngles = new Vector3(0, 0, 180);
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
 
+        if (goWhatISee != null)
+        {
+            //TODO Lo queremos como E o como Espacio??
+            if (canGetObjects() != "" && Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Puedo coger esto: " + goWhatISee.tag);
+                Destroy(goWhatISee);
+            }
+        }
 
     }
+
+    public string canGetObjects()
+    {
+        string result = "";
+
+        Vector2 targetDirection = goWhatISee.transform.position - transform.position;
+        Vector2 forward = transform.up;
+        float distance = Vector2.Distance(goWhatISee.transform.position, transform.position);
+
+        if (Vector2.Angle(forward, targetDirection) <= visionAngle && distance <= visionDistance)
+        {
+            result = goWhatISee.tag;
+        }
+
+        return result;
+    }
+
+    ////Para BORRAR
+    //public void FixedUpdate()
+    //{
+    //    RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+    //    if (hit.collider.tag != "Untagged")
+    //    {
+    //        Debug.Log("Veo un: " + hit.collider.tag);
+    //    }
+    //}
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        //TODO Esto lo podemos cambiar a !="Untagged" y después según lo que sea pues lo cogemos, o desaparece o lo que se quiera
         if (collision.gameObject.tag == "Joya")
         {
-            Destroy(collision.gameObject);
+            //Destroy(collision.gameObject);
+            Debug.Log("Dentro del colision");
+            goWhatISee = collision.gameObject;
         }
     }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Fuera de colision");
+        goWhatISee = null;
+    }
+    
 }
