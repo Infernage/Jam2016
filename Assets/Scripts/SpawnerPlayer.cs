76e7aae2e7901be3018ebbe5c55373131a50f38e
Alberto <albertodelcru@gmail.com>
2016-07-09 23:05:55 +0200

    PlayerSpawn

---
+++ Assets/Scripts/SpawnerPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerPlayer : MonoBehaviour
+{
+
+    public GameObject female, male;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (PlayerPrefs.GetString("character").Equals("female"))
+        {
+            Instantiate(female, this.transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(male, this.transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
