a91b29c2d0bf0c4365f532fa6146c8aaf61f1860
Alberto <albertodelcru@gmail.com>
2016-07-09 22:39:22 +0200

    Pick ups generator

---
+++ Assets/Scripts/PicksUpsGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PicksUpsGenerator : MonoBehaviour
+{
+
+    public GameObject[] pickUps;
+    
+    // Use this for initialization
+    void Start()
+    {
+        Instantiate(pickUps[Random.Range(0,pickUps.Length-1)],this.transform.position,Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
